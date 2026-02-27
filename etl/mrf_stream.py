72c995ea7d3a9b5e304823a59a49e75ccf3883be
RussellStover1983 <russell.stover@gmail.com>
2026-02-27 14:20:58 -0600

    Debug: print first price_entry to stdout

--- etl/mrf_stream.py
+++ etl/mrf_stream.py
@@ -277,13 +277,10 @@ class MrfStreamProcessor:
                     for price_entry in neg_rate_entry.get("negotiated_prices", []):
                         neg_rate = price_entry.get("negotiated_rate", 0.0)
 
-                        # Debug: log first price entry structure
+                        # Debug: print first price entry structure to stdout
                         if self.result.iowa_rates_extracted == 0:
-                            logger.info(
-                                "First price_entry keys: %s, sample: %s",
-                                list(price_entry.keys()),
-                                {k: v for k, v in list(price_entry.items())[:6]},
-                            )
+                            import json as _json
+                            print(f"DEBUG_PRICE_ENTRY: {_json.dumps(price_entry, default=str)[:500]}", flush=True)
                         neg_type = price_entry.get("negotiated_type", "")
                         service_codes = price_entry.get("service_code", [])
                         billing_class = price_entry.get("billing_class", "")
